e0e355e327fc7b397122f8c77bbcc76586b5820a
Pablo Perez <perez51160900@hotmail.com>
2022-01-03 14:13:56 -0400

    login done

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -6,15 +6,24 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  final _apikey = 'AIzaSyBujjzJ9QgDoUgZ2pcuIXqqhaDK3X8NEpo';
 
-  Future<void> signup(String email, String password) async {
-    final apikey = 'AIzaSyBujjzJ9QgDoUgZ2pcuIXqqhaDK3X8NEpo';
-    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apikey');
+  Future<void> _authenticate(String email, String password, String action) async {
+    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:$action?key=$_apikey');
     final response = await http.post(url, body: json.encode({
       'email': email,
       'password': password,
       'returnSecureToken': true,
     }) );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+
+  }
+  
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
